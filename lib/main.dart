9c70b580c920e4cf1611ff30632f5c4b04610b15
mind <skyphantomhz@gmail.com>
2019-09-06 00:04:10 +0700

    base stack

--- lib/main.dart
+++ lib/main.dart
@@ -28,8 +28,126 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        
+      body: Container(
+        decoration: BoxDecoration(color: Color(0xfff4f4fb)),
+        child: Column(
+          children: <Widget>[
+            Stack(
+              overflow: Overflow.visible,
+              children: <Widget>[
+                Container(
+                  height: 220.0,
+                  margin: EdgeInsets.fromLTRB(60.0, 60.0, 0, 0),
+                  decoration: BoxDecoration(color: Colors.white),
+                ),
+                Positioned(
+                  left: 45.0,
+                  top: 45.0,
+                  child: Container(
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.black26,
+                              blurRadius: 10.0,
+                              offset: Offset(0, 4.0))
+                        ]),
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.arrow_back,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  right: 30.0,
+                  top: 45.0,
+                  child: Stack(
+                    alignment: Alignment.topRight,
+                    overflow: Overflow.visible,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(color: Colors.black12, blurRadius: 1.0)
+                            ]),
+                        child: IconButton(
+                          icon: Icon(
+                            Icons.favorite_border,
+                            color: Color(0xff3f4cdE),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        top: -7.0,
+                        child: Container(
+                          padding: EdgeInsets.all(5.0),
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle, color: Color(0xff3f4cdE)),
+                          child: Text(
+                            "2",
+                            style: TextStyle(color: Colors.white, fontSize: 10),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                // cost and review star
+                Positioned(
+                  top: 300.0,
+                  right: 35.0,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Text(
+                        "\$84",
+                        style: TextStyle(
+                            fontFamily: 'Sofia',
+                            fontSize: 30.0,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        "Review :",
+                        style: TextStyle(fontFamily: 'Sofia'),
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Icon(
+                            Icons.star,
+                            size: 10.0,
+                          ),
+                          Icon(
+                            Icons.star,
+                            size: 10.0,
+                          ),
+                          Icon(
+                            Icons.star,
+                            size: 10.0,
+                          ),
+                          Icon(
+                            Icons.star,
+                            size: 10.0,
+                          ),
+                          Icon(
+                            Icons.star,
+                            size: 10.0,
+                            color: Colors.black26,
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
