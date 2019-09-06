1958e2aaa5d0db4f897d267c45668c09515593c7
DacTri <tri.trandac@axonactive.com>
2019-09-06 17:16:43 +0700

    second

--- lib/main.dart
+++ lib/main.dart
@@ -31,14 +31,70 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Container(
         decoration: BoxDecoration(color: Color(0xfff4f4fb)),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
           children: <Widget>[
             Stack(
               overflow: Overflow.visible,
               children: <Widget>[
-                Container(
-                  height: 220.0,
-                  margin: EdgeInsets.fromLTRB(60.0, 60.0, 0, 0),
-                  decoration: BoxDecoration(color: Colors.white),
+                Column(
+                  children: <Widget>[
+                    Container(
+                      height: 220.0,
+                      margin: EdgeInsets.fromLTRB(60.0, 60.0, 0, 0),
+                      decoration: BoxDecoration(color: Colors.white),
+                    ),
+                    Container(
+                      alignment: Alignment(0.75, 0.0),
+                      margin: EdgeInsets.only(top: 20.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Text(
+                            "\$84",
+                            style: TextStyle(
+                                fontFamily: 'Sofia-Pro',
+                                fontSize: 30.0,
+                                fontWeight: FontWeight.w700),
+                          ),
+                          Text(
+                            "Review :",
+                            style: TextStyle(
+                                fontFamily: 'Sofia-Pro',
+                                fontWeight: FontWeight.w800),
+                          ),
+                          Row(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: <Widget>[
+                              Icon(
+                                Icons.star,
+                                size: 10.0,
+                              ),
+                              Icon(
+                                Icons.star,
+                                size: 10.0,
+                              ),
+                              Icon(
+                                Icons.star,
+                                size: 10.0,
+                              ),
+                              Icon(
+                                Icons.star,
+                                size: 10.0,
+                              ),
+                              Icon(
+                                Icons.star,
+                                size: 10.0,
+                                color: Colors.black26,
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
                 Positioned(
                   left: 45.0,
@@ -98,56 +154,85 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                 ),
                 // cost and review star
-                Positioned(
-                  top: 300.0,
-                  right: 35.0,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      Text(
-                        "\$84",
-                        style: TextStyle(
-                            fontFamily: 'Sofia',
-                            fontSize: 30.0,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        "Review :",
-                        style: TextStyle(fontFamily: 'Sofia'),
-                      ),
-                      Row(
-                        children: <Widget>[
-                          Icon(
-                            Icons.star,
-                            size: 10.0,
-                          ),
-                          Icon(
-                            Icons.star,
-                            size: 10.0,
-                          ),
-                          Icon(
-                            Icons.star,
-                            size: 10.0,
-                          ),
-                          Icon(
-                            Icons.star,
-                            size: 10.0,
-                          ),
-                          Icon(
-                            Icons.star,
-                            size: 10.0,
-                            color: Colors.black26,
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                )
               ],
             ),
+            Container(
+              margin: EdgeInsets.fromLTRB(60.0, 20.0, 0.0, 0.0),
+              child: Text(
+                "Material: Textile",
+                style: TextStyle(
+                    fontFamily: 'Sofia-Pro', fontWeight: FontWeight.w800),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(60.0, 10.0, 30.0, 0.0),
+              child: Text(
+                "Size & Fit : This brand runs true to size to ensure the best fit",
+                softWrap: true,
+                style: TextStyle(
+                    fontFamily: 'Sofia-Pro', fontWeight: FontWeight.w800),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(60.0, 20.0, 0, 20.0),
+              child: Row(
+                children: <Widget>[
+                  _sizeSelection("6"),
+                  _sizeSelection("7"),
+                  _sizeSelection("8"),
+                  _sizeSelection("9"),
+                  Container(
+                    width: 2.0,
+                    height: 50.0,
+                    margin: EdgeInsets.fromLTRB(10, 0, 20, 0),
+                    decoration:
+                        BoxDecoration(color: Color.fromRGBO(32, 24, 27, 1)),
+                  ),
+                  _colorSelection(0xff333335),
+                  _colorSelection(0xff3f4cde),
+                ],
+              ),
+            )
           ],
         ),
+      ),
+    );
+  }
+
+  String sizeSelected = "6";
+  Widget _sizeSelection(String size) {
+    return GestureDetector(
+      onTap: () {
+        if (sizeSelected != size)
+          setState(() {
+            sizeSelected = size;
+          });
+      },
+      child: Container(
+        padding: EdgeInsets.all(15),
+        margin: EdgeInsets.only(right: 10.0),
+        decoration: BoxDecoration(
+            color: sizeSelected == size
+                ? Colors.white
+                : Color.fromRGBO(238, 238, 255, 1),
+            borderRadius: BorderRadius.all(Radius.circular(10))),
+        child: Text(size,
+            style: TextStyle(
+                fontFamily: 'Sofia-Pro', fontWeight: FontWeight.w800)),
+      ),
+    );
+  }
+
+  Widget _colorSelection(int color) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      margin: EdgeInsets.only(right: 10),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          color: Colors.white),
+      child: Container(
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(shape: BoxShape.circle, color: Color(color)),
       ),
     );
   }
